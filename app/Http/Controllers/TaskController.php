d4d8409e913561980d0fcc6575d5c7f1d20546ab
Dedi <id.dedi81@gmail.com>
2023-07-23 14:50:36 +0700

    Model Task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,19 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
     public function index() {
         return view('tasks.index', [
-            'tasks' => DB::table('tasks')->orderBy('id', 'desc')->get(),
+            // 'tasks' => DB::table('tasks')->orderBy('id', 'desc')->get(),
+            'tasks' => Task::orderBy('id', 'desc')->get(),
         ]);
     }
 
     public function store(Request $request) {
-        DB::table('tasks')->insert([
+        // DB::table('tasks')->insert([
+        //     'list' => $request->list,
+        // ]);
+        // Task::insert([
+        //     'list' => $request->list,
+        // ]);
+        Task::create([
             'list' => $request->list,
         ]);
 
@@ -22,18 +30,21 @@ class TaskController extends Controller
     }
     
     public function edit($id) {
-        $task = DB::table('tasks')->where('id', $id)->first();
+        // $task = DB::table('tasks')->where('id', $id)->first();
+        $task = Task::find($id);
 
         return view('tasks/edit', compact('task'));
     }
 
     public function update(Request $request, $id) {
-        DB::table('tasks')->where('id', $id)->update(['list' => $request->list]);
+        // DB::table('tasks')->where('id', $id)->update(['list' => $request->list]);
+        Task::find($id)->update(['list' => $request->list]);
         return redirect('/tasks');
     }
 
     public function destroy($id) {
-        DB::table('tasks')->where('id', $id)->delete();
+        // DB::table('tasks')->where('id', $id)->delete();
+        Task::find($id)->delete();
         return back();
     }
 }
