efc63523ba0e97d34afc5ce6d9e197117ef929d3
Dedi <id.dedi81@gmail.com>
2023-07-24 11:14:14 +0700

    add bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}} | Example</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     {{$styles}}
 </head>
 <body>
     <x-navbar></x-navbar>
-    {{$slot}}
-<script src="{{asset('js/app.js')}}"></script>
+    <div class="container">
+        {{$slot}}
+    </div>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
 </body>
 </html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,25 +1,61 @@
 <x-app-layout title="Tasks">
-    <h1>List Task</h1>
-    <div>
-        <form action="/tasks" method="post">
-            @csrf
-            <input type="text" name="list" placeholder="Add new task">
-            <input type="submit" value="Add">
-        </form>
-    </div>
-    <ol>
-        @foreach ($tasks as $task )
-            <li style="margin-bottom: 10px;">
-                {{$task->list}}
-                <div>
-                    <a href="/tasks/{{$task->id}}/edit">Edit</a>
-                    <form action="/tasks/{{$task->id}}" method="post" style="display: inline; margin-left: 5px">
+    <div class="row">
+        <div class="col-6">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h1>Create New Task</h1>
+                </div>
+                <div class="card-body">
+                    <form action="/tasks" method="post" class="d-flex justify-content-between">
                         @csrf
-                        @method('delete')
-                        <input type="submit" value="Delete">
+                        <input type="text" name="list" placeholder="Add new task" class="form-control me-2">
+                        <input type="submit" value="Add" class="btn btn-primary">
                     </form>
                 </div>
-            </li>
+            </div>
+        </div>
+    </div>
+
+    <table class="table table-striped table-hover table-bordered">
+        <thead>
+            <tr>
+                <th scope="col" class="text-center">No.</th>
+                <th scope="col" class="text-center">Task</th>
+                <th scope="col" class="text-center">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tasks as $index => $task )
+            <tr>
+                <td class="align-middle text-center" scope="row">{{$index + 1}}</td>
+                <td class="align-middle">{{$task}}</td>
+                <td class="align-middle">
+                    <div class="d-flex">
+                        <a href="/tasks/{{$task->id}}/edit" class="me-2 btn btn-warning">Edit</a>
+                        <form action="/tasks/{{$task->id}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <input type="submit" value="Delete" class="btn btn-danger">
+                        </form>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <!-- <ol>
+        @foreach ($tasks as $task )
+        <li style="margin-bottom: 10px;">
+            {{$task->list}}
+            <div>
+                <a href="/tasks/{{$task->id}}/edit">Edit</a>
+                <form action="/tasks/{{$task->id}}" method="post" style="display: inline; margin-left: 5px">
+                    @csrf
+                    @method('delete')
+                    <input type="submit" value="Delete">
+                </form>
+            </div>
+        </li>
         @endforeach
-    </ol>
+    </ol> -->
 </x-app-layout>
